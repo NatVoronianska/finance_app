25da7c4a4c37b6a874d96e4d2c738066cdbc93ef
Natalia Voronjanska <voronjanskaja@gmail.com>
2023-01-16 20:07:56 +0200

    add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :operations
+
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,6 @@
 class Operation < ApplicationRecord
   belongs_to :category
+
+  validates :amount, :date, :description, presence: true
+  validates :amount, numericality: {greater_than 0: true }
 end
