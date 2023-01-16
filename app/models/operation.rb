1892aae62c01dafcc5c21ee06ac64ed6c92c61e6
Natalia Voronjanska <voronjanskaja@gmail.com>
2023-01-16 21:11:04 +0200

    upd validation

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,5 +2,5 @@ class Operation < ApplicationRecord
   belongs_to :category
 
   validates :amount, :date, :description, presence: true
-  validates :amount, numericality: {greater_than 0: true }
+  validates :amount, numericality: { greater_than: 0 }
 end
