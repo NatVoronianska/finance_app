5ac2061f6cd63b93c438fa19a6d78b82e987b84e
Natalia Voronjanska <voronjanskaja@gmail.com>
2023-02-21 16:21:49 +0200

    report controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+  get 'reports/report_by_category'
+  get 'reports/report_by_dates'
+  get 'main/index'
+  get 'reports/the_action/:some_param', to: 'report#report_by_category', prm: 'the_prm'
+  get 'reports/the_action/:some_param', to: 'report#report_by_dates', prm: 'the_prm'
+
   resources :operations
   resources :categories
+  root "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
